9181999a372a9ce11e3ee9cf1c102e28f16e8e8d
nguyenrio <nguyendc23v7x107@dttx.ctu.edu.vn>
2025-04-22 21:31:46 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -11,30 +11,37 @@
             <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts"
                 v-model:activeIndex="activeIndex" />
             <p v-else>Không có liên hệ nào.</p>
-        <div class="mt-3 row justify-content-around align-items-center">
-            <button class="btn btn-sm btn-primary" @click="refreshList()">
-                <i class="fas fa-redo"></i> Làm mới
-            </button>
-            <button class="btn btn-sm btn-success" @click="goToAddContact">
-                <i class="fas fa-plus"></i> Thêm mới
-            </button>
-    
-             <button class="btn btn-sm btn-danger" @click="removeAllContacts">
-                        <i class="fas fa-trash"></i> Xóa tất cả
-                    </button>
-                </div>
-                </div>
-                <div class="mt-3 col-md-6">
-                    <div v-if="activeContact">
-                        <h4>
-                            Chi tiết Liên hệ
-                            <i class="fas fa-address-card"></i>
-                        </h4>
-                        <ContactCard :contact="activeContact" />
-                    </div>
-                </div>
+            <div class="mt-3 row justify-content-around align-items-center">
+                <button class="btn btn-sm btn-primary" @click="refreshList()">
+                    <i class="fas fa-redo"></i> Làm mới
+                </button>
+                <button class="btn btn-sm btn-success" @click="goToAddContact">
+                    <i class="fas fa-plus"></i> Thêm mới
+                </button>
+
+                <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+                    <i class="fas fa-trash"></i> Xóa tất cả
+                </button>
             </div>
-            </template>
+        </div>
+        <div class="mt-3 col-md-6">
+            <div v-if="activeContact">
+                <h4>
+                    Chi tiết Liên hệ
+                    <i class="fas fa-address-card"></i>
+                </h4>
+                <ContactCard :contact="activeContact" />
+                <router-link :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
 
             <script>
 import ContactCard from "@/components/ContactCard.vue";
