cc17ca63411972d057de4dda8f15c38b30de67fe
nguyenrio <nguyendc23v7x107@dttx.ctu.edu.vn>
2025-04-22 21:57:40 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,36 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
-    },
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền $route.params dưới dạng props vào component
+  },
 
-    {
-  path: "/:pathMatch(.*)*",
-  name: "notfound",
-  component: () => import("@/views/NotFound.vue"),
-    },
-    
-    {
-  path: "/contacts/:id",
-  name: "contact.edit",
-  component: () => import("@/views/ContactEdit.vue"),
-  props: true, // Truyền các biến trong $route.params vào làm props
-}
+  {
+    path: "/contacts/new",
+    name: "contact.add",
+    component: () => import("@/views/ContactForm.vue"),
+    props: { id: null }, // Đặt id = null khi thêm mới
+  },
+
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory("/"), // Đảm bảo BASE_URL có giá trị mặc định là '/'
+  routes,
 });
 
 export default router;
